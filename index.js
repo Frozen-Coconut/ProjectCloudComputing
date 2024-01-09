c0f04c4cd00d207ab30ec0991acce8e574ffd663
leonard3368 <51233664+leonard3368@users.noreply.github.com>
2024-01-10 00:14:14 +0700

    fixed library

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const CharacterAI = require('node_characterai');
+const CharacterAI = require('./node_characterai');
 const characterAI = new CharacterAI();
 
 const cors = require('cors');
